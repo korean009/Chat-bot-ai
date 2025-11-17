7ecfbfbbf429bbc39ee1d4c128cf356d2b5c436f
sidhubad143 <lakhwinder4332@gmail.com>
2025-11-17 16:44:57 +0530

    Update __main__.py

--- shizuchat/__main__.py
+++ shizuchat/__main__.py
@@ -17,49 +17,53 @@ async def anony_boot():
         LOGGER.error(ex)
         sys.exit(1)
 
+    # Import all modules
     for all_module in ALL_MODULES:
         importlib.import_module("shizuchat.modules." + all_module)
-        LOGGER.info(f"ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ: {all_module}")
+        LOGGER.info(f"Successfully Imported: {all_module}")
 
     # Set bot commands
     try:
         await shizuchat.set_bot_commands(
             commands=[
-                BotCommand("start", "✧ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ✧"),
-                BotCommand("help", "✧ ɢᴇᴛ ᴛʜᴇ ʜᴇʟᴘ ᴍᴇɴᴜ ✧"),
-                BotCommand("ping", "✧ ᴄʜᴇᴄᴋ ɪғ ᴛʜᴇ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇsᴅ ✧"),
-                BotCommand("shipping", "✧ ᴄᴏᴜᴘʟᴇs ᴏғ ᴅᴀʏ ✧"),
-                BotCommand("rankings", "✧ ᴜsᴇʀ ᴍsɢ ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ ✧"),
+                BotCommand("start", "Start the bot"),
+                BotCommand("help", "Help menu"),
+                BotCommand("ping", "Bot status"),
+                BotCommand("shipping", "Couples of the day"),
+                BotCommand("rankings", "User message leaderboard"),
             ]
         )
-        LOGGER.info("ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅꜱ ꜱᴇᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ.")
+        LOGGER.info("Bot Commands Set Successfully.")
     except Exception as ex:
-        LOGGER.error(f"ꜰᴀɪʟᴇᴅ ᴛᴏ ꜱᴇᴛ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅꜱ: {ex}")
+        LOGGER.error(f"Failed to set bot commands: {ex}")
 
     LOGGER.info(f"@{shizuchat.username} Started.")
+
     try:
         await shizuchat.send_message(int(OWNER_ID), f"{shizuchat.mention} has started")
-    except Exception as ex:
-        LOGGER.info(f"@{shizuchat.username} ꜱᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀꜱᴇ ꜱᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ꜰʀᴏᴍ ᴏᴡɴᴇʀ ɪᴅ.")
-    
+    except Exception:
+        LOGGER.info("Start the bot from owner ID first.")
+
     await idle()
 
 
-# Flask Server Code for Health Check
+# Flask server for health check
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Bot is running"
 
+
 def run_flask():
     app.run(host="0.0.0.0", port=8000)
 
-if __name__ == "__main__":
-    # Start Flask server in a new thread
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.start()
 
-    # Start the bot asynchronously
-    asyncio.get_event_loop().run_until_complete(anony_boot())
-    LOGGER.info("ꜱᴛᴏᴘᴘɪɴɢ ꜱʜɪᴢᴜᴄʜᴀᴛ ʙᴏᴛ...")
+if __name__ == "__main__":
+    # Start Flask in another thread
+    threading.Thread(target=run_flask).start()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(anony_boot())
+
+    LOGGER.info("Stopping shizuchat bot...")
